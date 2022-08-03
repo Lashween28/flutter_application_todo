0f8ea37a6180d2b056fd8308581d91c1e09faa7f
lashween <lashweenraj@besquare.com.my>
2022-08-03 17:54:35 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+// This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData.light(),
+      home: const MyHomePage(title: 'TO-DO List'),
+    );
+  }
+}
+
+class Todo {
+  String? title;
+  String? description;
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final _textDescriptionController = TextEditingController();
+  final _textTitleController = TextEditingController();
+  final List<Todo> todoList = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Column(
+// mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+// color: Colors.red,
+              padding: EdgeInsets.all(20),
+              child: TextField(
+                controller: _textTitleController,
+                decoration: InputDecoration(
+                  focusedBorder: OutlineInputBorder(
+                    borderSide:
+                        BorderSide(color: Colors.blue, width: 3.0),
+                  ),
+                  hintText: "Title",
+                  border: OutlineInputBorder(),
+                ),
+              )),
+          Container(
+// color: Colors.red,
+              padding: EdgeInsets.all(20),
+              child: TextField(
+                controller: _textDescriptionController,
+                decoration: InputDecoration(
+                  focusedBorder: OutlineInputBorder(
+                    borderSide:
+                        BorderSide(color: Colors.blue, width: 3.0),
+                  ),
+                  hintText: "Description",
+                  border: OutlineInputBorder(),
+                ),
+              )),
+          Expanded(
+            child: ListView.builder(
+              shrinkWrap: true,
+              itemCount: todoList.length,
+              itemBuilder: (BuildContext context, int index) {
+                if (index.isOdd) {
+                  return Divider(color: Colors.blue);
+                }
+                return ListTile(
+                  title: Text(
+                    // ('Todo-${index+1}')
+                    todoList[index].title ?? '',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold, color: Colors.blue[400]),
+                  ),
+                  subtitle: Text(
+                    todoList[index].description ?? 'empty',
+                    style: TextStyle(color: Colors.blue[100]),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.red[400],
+        onPressed: () {
+          Todo todo_Item = Todo();
+          todo_Item.title = _textTitleController.text;
+          todo_Item.description = _textDescriptionController.text;
+          setState(() {
+            todoList.add(todo_Item);
+          });
+        },
+        tooltip: 'Add Task',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
